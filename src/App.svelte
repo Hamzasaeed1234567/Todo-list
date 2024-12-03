d41aa2ccee7e105e1b7a15865e5eede6762f2822
Hamzasaeed1234567 <hamza.saeed@packagex.io>
2024-12-03 16:48:42 +0500

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Todo from './Todo.svelte';
+</script>
+
+<main>
+  <Todo />
+</main>
+
+<style>
+  main {
+    font-family: Arial, sans-serif;
+    margin: 20px;
+  }
+</style>
---
+++ src/todo.svelte
@@ -0,0 +1,44 @@
+<script>
+  let todos = [];
+  let newTodo = "";
+
+
+  function addTodo() {
+    if (newTodo.trim()) {
+      todos = [...todos, { text: newTodo }];
+      newTodo = "";
+    }
+  }
+
+
+
+
+  function deleteTodo(index) {
+    todos = todos.filter((_, i) => i !== index);
+  }
+</script>
+
+<style>
+
+</style>
+
+<h1>To-Do List</h1>
+
+
+<input
+  type="text"
+  bind:value={newTodo}
+  placeholder="Add a new value..."
+/>
+<button on:click={addTodo}>Add</button>
+
+
+<ul>
+  {#each todos as todo, index}
+    <li>
+  
+      <span >{todo.text}</span>
+      <button on:click={() => deleteTodo(index)}>Delete</button>
+    </li>
+  {/each}
+</ul>
